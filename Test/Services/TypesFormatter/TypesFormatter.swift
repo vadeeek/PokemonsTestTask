a7a4754830481c6c6f9d8f78c570f05316e1e2cb
Vadim Velichko <super.velik.123@mail.ru>
2024-05-03 04:03:31 +0300

    [POKEMON-22] Обновить TypeFormatter
    Сделать поддержку под type покемона, category предмета и их имена

--- Test/Services/TypesFormatter/TypesFormatter.swift
+++ Test/Services/TypesFormatter/TypesFormatter.swift
@@ -2,11 +2,16 @@ import UIKit
 
 final class TypeFormatter {
     
+    enum InputType {
+        case name(string: String)
+        case itemCategory(string: String)
+    }
+    
     // MARK: - Properties
     static let shared = TypeFormatter()
     
     // MARK: - Methods
-    func formate(typeString: String) -> (String, UIColor) {
+    func format(typeString: String) -> (String, UIColor) {
         let typesStrings: [String] = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy", "unknown", "shadow"]
         let typesColors: [UIColor] = [.lightGray, .orange, .systemBlue, .systemPink, .systemBrown, .systemGray3, .systemGreen, .purple, .lightGray, .orange, .systemBlue, .green, .yellow, .white, .white, .black, .darkGray, .systemPink, .lightGray, .systemGray4]
         
@@ -18,6 +23,20 @@ final class TypeFormatter {
             }
         } else {
             return (typeString.capitalized, .lightGray)
+        }
+    }
+    
+    func format(type: InputType) -> String {
+        switch type {
+        case .name(let string):
+            let removedHyphens = string.replacingOccurrences(of: "-", with: " ")
+            let capitalizedString = removedHyphens.capitalized
+            return capitalizedString
+        case .itemCategory(let string):
+            let removedHyphens = string.replacingOccurrences(of: "-", with: " ")
+            let lowercasedString = removedHyphens.lowercased()
+            let capitalizedFirstLetter = lowercasedString.prefix(1).capitalized + lowercasedString.dropFirst()
+            return capitalizedFirstLetter
         }
     }
 }
